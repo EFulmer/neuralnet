142fe7929619214acbbc1c9b2d6ec2d945dc6599
Eric Fulmer <eric.r.fulmer@gmail.com>
2019-03-10 10:46:36 -0400

    relu

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+/// Standard Rectified Linear Unit (ReLU) function.
+///
+/// See also: https://medium.com/tinymind/a-practical-guide-to-relu-b83ca804f1f7
+///
+/// # Examples
+///
+/// ```
+/// assert!(relu(3.14) == 3.14)
+/// assert!(relu(-1.9) == 0.0)
+/// ```
+fn relu(x: f32) -> f32 {
+    x.max(0.0)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn relu_pos() {
+        assert_eq!(relu(1.0), 1.0)
+    }
+    fn relu_neg() {
+        assert_eq!(relu(-1.0), 0.0)
     }
 }
